49783f7ab7e3895c2597359fb08118b8360f9c86
Lê Hồng Thắng <lhth@netcompany.com>
2024-03-19 22:22:32 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by lhth on 19/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
