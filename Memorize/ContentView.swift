9d6f9d7426d61b16737961776123d77ac1419a43
Lê Hồng Thắng <thangle9804@gmail.com>
2024-04-09 00:58:12 +0700

    feat(main-view) update card view, refactor, make card list scrollable

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,28 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        let emojis = ["🎃", "😈", "👻", "💀"]
+    @State var cardCount: Int = 4
+    let emojis = ["✌", "😂", "😝", "😁", "😱", "👉", "🙌", "🍻", "🔥", "🌈", "☀", "🎈", "🌹", "💄", "🎀", "⚽", "🎾", "🏁", "😡", "👿", "🐻", "🐶", "🐬", "🐟", "🍀", "👀", "🚗", "🍎", "💝", "💙", "👌", "❤", "😍", "😉", "😓", "😳", "💪", "💩", "🍸", "🔑", "💖", "🌟", "🎉", "🌺", "🎶", "👠", "🏈", "⚾", "🏆", "👽", "💀", "🐵", "🐮", "🐩", "🐎", "💣", "👃", "👂", "🍓", "💘", "💜", "👊", "💋", "😘", "😜", "😵", "🙏", "👋", "🚽", "💃", "💎", "🚀", "🌙", "🎁", "⛄", "🌊", "⛵", "🏀", "🎱", "💰", "👶", "👸", "🐰", "🐷", "🐍", "🐫", "🔫", "👄", "🚲", "🍉", "💛", "💚"]
 
+    var body: some View {
         VStack {
-            HStack {
-                ForEach(0 ..< emojis.count, id: \.self) { index in
-                    CardView(content: emojis[index])
-                }
+            ScrollView {
+                cards
             }
-            .foregroundColor(.orange)
-            .padding()
-            HStack {
-                Button("add card", action: {
-                    print("ADD card clicked")
-                })
-                Spacer()
-                Button("remove card", action: {
-                    print("REMOVE card clickee")
-                })
-            }.buttonStyle(.borderedProminent)
-                .padding([.leading, .trailing])
+            Spacer()
+            cardsModifier
         }
+    }
+
+    var cardsModifier: some View {
+        HStack {
+            cardAdderButton
+            Spacer()
+            cardRemoverButton
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+        .padding([.leading, .trailing])
+    }
+
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 180))]) {
+            ForEach(0 ..< cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2 / 3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+
+    func cardCountModifier(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+
+    var cardAdderButton: some View {
+        cardCountModifier(by: +1, symbol: "rectangle.stack.fill.badge.plus")
+    }
+
+    var cardRemoverButton: some View {
+        cardCountModifier(by: -1, symbol: "rectangle.stack.fill.badge.minus")
     }
 }
 
@@ -39,14 +67,15 @@ struct CardView: View {
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
-                base.foregroundColor(.white)
+            Group {
+                base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                base.foregroundColor(.orange)
             }
+            .opacity(isFaceUp ? 0 : 1)
+            base.fill(.orange).opacity(isFaceUp ? 1 : 0)
         }
+        .shadow(radius: 10)
         .onTapGesture {
             isFaceUp.toggle()
         }
